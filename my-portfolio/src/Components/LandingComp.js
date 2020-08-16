f011e183e4456fd4c6c3ab62fb613dac873d37b1
Sonal240 <simranshilky@gmail.com>
2020-08-16 18:45:59 +0530

    added material ui core comp and dummy pages
    design is partially ready...empty js files...

---
+++ my-portfolio/src/Components/LandingComp.js
@@ -0,0 +1,10 @@
+import {React,Component} from 'react'
+import {Button} from '@material-ui/core'
+
+class LandingComp extends Component{
+render(){
+  return ( <p>HI</p>);
+}
+}
+
+export default LandingComp;
