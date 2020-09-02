6465f1bf8675fa6819afa488d0f7463ad62230df
Sonal240 <simranshilky@gmail.com>
2020-09-02 10:29:39 +0530

    logo added navbar yet to be developed

--- my-portfolio/src/Components/Navabar.js
+++ my-portfolio/src/Components/Navabar.js
@@ -1,45 +1,39 @@
-import React,{Component} from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import logo from 'logo.svg';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(0),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-const classes = useStyles();
+export default function NavbarComp() {
+  const classes = useStyles();
 
-
-class NavbarComp extends Component{
-    render(){
-        return(
-            <div className={classes.root}>
+  return (
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <logo />
+          </IconButton>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
-        );
-    }
+  );
 }
-
-export default NavbarComp;
